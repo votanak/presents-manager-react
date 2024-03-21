ca89b84187ed825ab331dd96ee4c47f61a242ceb
votanak <v0tanak@yandex.ru>
2024-03-21 23:40:11 +0300

    excel in progress

---
+++ client/src/services/jsonToWb.js
@@ -0,0 +1,105 @@
+export const jsonToWb = (selectedGoods, giftId) => {
+  const wb = new Excel.Workbook();
+  const ws = wb.addWorksheet('My Sheet', {
+    views: [{ state: 'frozen', xSplit: 1, ySplit: 1 }],
+  });
+
+  ws.getRow(1).values = ['Сборный подарок'];
+  ws.getRow(3).values = [
+    'Заказ',
+    `№${giftId} от ${new Date().toLocaleString('ru', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      timezone: 'UTC',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    })}`,
+  ];
+  ws.getRow(5).values = [
+    'Артикул',
+    'Наименование',
+    'Кол-во, шт.',
+    'Вес, грамм',
+    'Стоимость, руб.',
+  ];
+  ws.getRow(6).values = [
+    selectedGoods.pack.id,
+    `Упаковка ${selectedGoods.pack.name} ${selectedGoods.pack.giftWeight} гр.`,
+    ,
+    ,
+    selectedGoods.pack.price1,
+  ];
+  Object.keys(selectedGoods)
+    .filter((el1) => el1 !== 'pack')
+    .forEach((el, ind) => {
+      ws.getRow(7 + ind).values = [
+        selectedGoods[el].good.id,
+        selectedGoods[el].good.name,
+        selectedGoods[el].quantity,
+        selectedGoods[el].quantity * +selectedGoods[el].good.weight1,
+        selectedGoods[el].quantity * +selectedGoods[el].good.price1,
+      ];
+    });
+
+  const sglength = Object.keys(selectedGoods).length;
+
+  ws.getRow(7 + sglength).values = [
+    '',
+    'Итого:',
+    '',
+    { formula: `SUM(D6:D${6 + sglength})` },
+    { formula: `SUM(E6:E${6 + sglength})` },
+  ];
+
+  ws.getRow(5).alignment = {
+    wrapText: true,
+    horizontal: 'center',
+    vertical: 'middle',
+  };
+  ws.getCell('A1').alignment = {
+    vertical: 'middle',
+  };
+  ws.getRow(1).alignment = {
+    horizontal: 'center',
+  };
+  ws.getRow('A6').alignment = {
+    horizontal: 'right',
+  };
+  ws.getCell(`A1`).font = {
+    bold: true,
+    size: 14,
+  };
+  ws.getCell(`B${7 + sglength}`).font = {
+    bold: true,
+  };
+  ws.getCell(`D${7 + sglength}`).font = {
+    bold: true,
+  };
+  ws.getCell(`E${7 + sglength}`).font = {
+    bold: true,
+  };
+
+  ws.columns = [
+    { width: 10 },
+    { width: 40 },
+    { width: 12 },
+    { width: 12 },
+    { width: 12 },
+  ];
+
+  ['A', 'B', 'C', 'D', 'E'].forEach((el) => {
+    for (i = 0; i < sglength + 3; i++) {
+      ws.getCell(`${el}${i + 5}`).border = {
+        top: { style: 'thin' },
+        left: { style: 'thin' },
+        bottom: { style: 'thin' },
+        right: { style: 'thin' },
+      };
+    }
+  });
+
+  ws.mergeCells('A1:E1');
+  return wb;
+};
--- server/writeXLSX.js
+++ server/writeXLSX.js
@@ -1,23 +1,11 @@
-let XLSX = require('xlsx');
+const Excel = require('exceljs');
+const jsonToWb = require('../client/src/services/jsonToWb');
 
 const writeXLSX = (reqBody) => {
-  let selectedGoods = JSON.parse(reqBody.selectedGoods);
-
-  let goods_to_write = Object.keys(selectedGoods)
-    .filter((el1) => el1 !== 'pack')
-    .map((el) => ({
-      Артикул: selectedGoods[el].good.id,
-      Наименование: selectedGoods[el].good.name,
-      Количество: selectedGoods[el].quantity,
-    }));
-
-  const workbook = XLSX.utils.book_new();
-  const worksheet = XLSX.utils.json_to_sheet([]);
-  XLSX.utils.sheet_add_json(worksheet, goods_to_write, {
-    origin: 'A5',
-  });
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'Заказ');
-  XLSX.writeFile(workbook, `./data/order_${reqBody.giftId}.xlsx`);
+  Excel.writeFile(
+    jsonToWb(JSON.parse(reqBody.selectedGoods)),
+    `./data/order_${reqBody.giftId}.xlsx`,
+  );
   return;
 };
 
