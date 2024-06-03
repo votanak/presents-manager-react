e3545d5eb485d11ed7f7608ddef181eea4a6f7a8
votanak <v0tanak@yandex.ru>
2024-06-04 01:18:14 +0300

    edit router paths

--- server/src/routes/giftRouter.js
+++ server/src/routes/giftRouter.js
@@ -12,15 +12,15 @@ import {
 
 const giftRouter = express.Router();
 
-giftRouter.post('/send_order', sendOrder);
-giftRouter.get('/get_json', getJson);
-giftRouter.post('/write_json', writeJson);
-giftRouter.post('/write_img', imgMulter.single('file'), writeImg);
-giftRouter.post('/login', login);
-giftRouter.post('/send_change_email', sendChangeEmail);
-giftRouter.get('/get_admin_data/:passUuid', getAdminData);
-giftRouter.post('/write_admin_data', writeAdminData);
-giftRouter.post('/get_token_silently', getTokenSilently);
-giftRouter.post('/logout', logout);
+giftRouter.post('/api/send_order', sendOrder);
+giftRouter.get('/api/get_json', getJson);
+giftRouter.post('/api/write_json', writeJson);
+giftRouter.post('/api/write_img', imgMulter.single('file'), writeImg);
+giftRouter.post('/api/login', login);
+giftRouter.post('/api/send_change_email', sendChangeEmail);
+giftRouter.get('/api/get_admin_data/:passUuid', getAdminData);
+giftRouter.post('/api/write_admin_data', writeAdminData);
+giftRouter.post('/api/get_token_silently', getTokenSilently);
+giftRouter.post('/api/logout', logout);
 
 export default giftRouter;
